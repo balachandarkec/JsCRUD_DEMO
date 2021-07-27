9c32d444f6262840b719c3b51003902bd3931ac3
Balachandar <balachandar.s@niit.com>
2021-07-27 12:48:53 +0530

    Inital Commit

---
+++ index.js
@@ -0,0 +1,58 @@
+console.log("Welcome to node project!");
+const url="http://localhost:3000/fruits";
+fetch(url)
+    .then(data=>{
+        return data.json();
+    })
+    .then(result=>{
+        console.log(result);
+        result.forEach(element => {
+           console.log( element.name );
+        });
+    });
+
+   // Collect All Form Data
+
+   let clearFrom=()=>{
+    document.querySelector("#addForm").reset();
+};
+
+var data_header={};
+   let collectData=()=>{
+    var data= {"id":document.querySelector("#id").value,
+    "name":document.querySelector("#name").value,
+    "price":document.querySelector("#price").value,
+    "qty":document.querySelector("#qty").value};
+
+
+   data_header={
+        method:'POST',
+        headers:{
+            'content-type':'application/json'
+        },
+        body:JSON.stringify(data)
+     };
+     clearFrom();
+   };
+
+
+   
+   
+
+    let createFruit=()=>{
+        collectData();
+      fetch(url,data_header)
+      .then(response=>{
+          if(!response.ok){
+              throw Error(response.status)
+          }
+          return response.json();
+      }).then((data)=>{
+          console.log(data);
+      })
+    }
+
+
+    let btn=document.querySelector("#btn-test");
+
+    btn.addEventListener("click", createFruit);
